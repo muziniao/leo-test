59f29d9e86f072b0ea2d2433e9cd9dbb7e0ccddb
muziniao@gmail.com <muziniao>
2014-03-17 15:36:02 +0800

    providerTest

---
+++ src/main/java/org/leo/ssl/ProviderTest.java
@@ -0,0 +1,30 @@
+package org.leo.ssl;
+
+import java.security.Provider;
+import java.security.Security;
+import java.util.Iterator;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import org.junit.Test;
+
+public class ProviderTest {
+
+	@Test
+	public void testProvider(){
+		Provider[] providers = Security.getProviders();
+        for (int i = 0; i < providers.length; i++) {
+            Provider provider = providers[i];
+            System.out.println("Provider name: " + provider.getName());
+            System.out.println("Provider information: " + provider.getInfo());
+            System.out.println("Provider version: " + provider.getVersion());
+            Set<Entry<Object, Object>> entries = provider.entrySet();
+            Iterator<Entry<Object, Object>> iterator = entries.iterator();
+            while (iterator.hasNext()) {
+                System.out.println("Property entry: " + iterator.next());
+            }
+            System.out.println("-----------------------------------------------------------");
+        }
+        
+	}
+}
